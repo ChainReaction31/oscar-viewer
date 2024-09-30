6a6bcebaee8429ea02e2052d44fdbf6520ad1506
Ian Patterson <cr31.dev@gmail.com>
2024-09-30 01:10:52 -0500

    partial success on charts to event-detail

--- src/app/_components/event-preview/EventPreview.tsx
+++ src/app/_components/event-preview/EventPreview.tsx
@@ -159,7 +159,7 @@ export function EventPreview() {
             </Stack>
             <ChartTimeHighlight gammaDatasources={gammaDatasources} neutronDatasources={neutronDatasources}
                                 thresholdDatasources={thresholdDatasources} occDatasources={occDatasources}
-                                setChartReady={setChartReady}/>
+                                setChartReady={setChartReady} modeType="preview"/>
             <LaneVideoPlayback videoDatasources={videoDatasources} setVideoReady={setVideoReady}
                                dataSynchronizer={syncRef.current}
                                addDataSource={setActiveVideoIDX}/>
